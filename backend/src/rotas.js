4b5c0a18e3d253564014aa4f63c62e09df2ae9bb
Guilherme Vaz <guivaz1993@gmail.com>
2022-04-08 17:22:43 -0300

    explicação das rotas criadas

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -6,12 +6,13 @@ const utilitarios = require('./controladores/utilitarios');
 
 const rotas = express();
 
+//rotas para manuseio do usuário
 rotas.get('/usuarios', usuarios.listarUsuarios);
 rotas.get('/usuarios/:id', usuarios.obterUsuario);
-rotas.post('/usuarios/:id', usuarios.addHabilidade);
-rotas.post('/usuarios/horario/:id', usuarios.addHorario);
-rotas.put('/usuarios/:id', usuarios.marcarMentoria);
+rotas.post('/usuarios/habilidades/:id', usuarios.addHabilidade);
+rotas.get('/usuarios/habilidades/:id', usuarios.listarHabilidades);
 
+// rotas para utilidades que não vamos usar no front, usei para testar a conexão com o banco de dados
 rotas.post('/habilidades', utilitarios.cadastrarHabilidade);
 
 module.exports = rotas
