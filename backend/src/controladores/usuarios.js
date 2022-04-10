dfba04ffd8b56edd0440d483b82ed4d5c3e4b03d
Felipe <felipemoreira1@gmail.com>
2022-04-10 01:28:25 -0300

    Implementação do acesso a Banco de Dados para 'usuarios' e 'forum'

--- backend/src/controladores/usuarios.js
+++ backend/src/controladores/usuarios.js
@@ -1,11 +1,36 @@
-const listarUsuarios = (req, res) => {
+const conexao = require('../conexao');
 
-    return res.status(200).json()
+const listarUsuarios = async (req, res) => {
+
+    try {
+        const usuarios = await conexao.query('SELECT * FROM usuario');
+
+        if (usuarios.rowCount === 0) {
+            return res.status(400).json('Não foi possível encontrar usuários')
+        }
+
+        res.status(200).send(usuarios.rows);
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json(error)
+    }
 }
 
-const obterUsuario = (req, res) => {
+const obterUsuario = async (req, res) => {
 
-    return res.status(200).json()
+    const {id} = req.params;
+    try {
+        const usuario = await conexao.query('SELECT * FROM usuario WHERE id = $1', [id]);
+
+        if (usuario.rowCount === 0) {
+            return res.status(400).json('Não foi possível encontrar o usuário')
+        }
+
+        res.status(200).send(usuario.rows);
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json(error)
+    }
 }
 
 const addHabilidade = (req, res) => {
@@ -21,16 +46,61 @@ const login = (req, res) => {
     return res.status(200).json()
 }
 
-const cadastrarUsuario = (req, res) => {
-    return res.status(200).json()
+const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha, bio, avatar, area } = req.body;
+
+    if (!nome || !senha || !area) {
+        return res.status(404).json('Dados obrigatórios não informados.')
+    }
+
+    try {
+        const novoUsuario = await conexao.query('INSERT INTO usuario (nome, email, senha, bio, avatar, area) VALUES ( $1, $2, $3, $4, $5, $6 )', 
+                                [nome, email, senha, bio, avatar, area]);
+
+        if (novoUsuario.rowCount === 0) {
+            return res.status(400).json('Não foi possível inserir o usuário')
+        }
+
+        res.status(201).json({ 'mensagem': 'Usuário Cadastrado' })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json(error)
+    }
 }
 
-const atualizarUsuario = (req, res) => {
-    return res.status(200).json()
+const atualizarUsuario = async (req, res) => {
+    const {id} = req.params;
+    const { nome, email, senha, bio, avatar, area } = req.body;
+
+    try {
+        const resposta = await conexao.query('UPDATE usuario SET nome = $1, email = $2, senha = $3, bio = $4, avatar = $5, area = $6 WHERE id = $7', 
+                                [nome, email, senha, bio, avatar, area, id]);
+
+        if (resposta.rowCount === 0) {
+            return res.status(400).json('Não foi possível atualizar o usuário')
+        }
+
+        res.status(200).json({ 'mensagem': 'Usuário atualizado com sucesso' })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json(error)
+    }
 }
 
-const deletarUsuario = (req, res) => {
-    return res.status(200).json()
+const deletarUsuario = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const usuario = await conexao.query('DELETE FROM usuario WHERE id = $1', [id]);
+
+        if (usuario.rowCount === 0) {
+            return res.status(400).json('Não foi possível excluir o usuário')
+        }
+
+        res.status(200).json({ 'mensagem': 'Usuário excluido com sucesso' })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json(error)
+    }
 }
 
 module.exports = {
--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const usuarios = require('./controladores/usuarios');
 const utilitarios = require('./controladores/utilitarios');
+const forum = require('./controladores/forum');
 
 
 
@@ -9,10 +10,21 @@ const rotas = express();
 //rotas para manuseio do usuário
 rotas.get('/usuarios', usuarios.listarUsuarios);
 rotas.get('/usuarios/:id', usuarios.obterUsuario);
+rotas.post('/usuarios/', usuarios.cadastrarUsuario);
+rotas.put('/usuarios/:id', usuarios.atualizarUsuario);
+rotas.delete('/usuarios/:id', usuarios.deletarUsuario);
 rotas.post('/usuarios/habilidades/:id', usuarios.addHabilidade);
 rotas.get('/usuarios/habilidades/:id', usuarios.listarHabilidades);
 
 // rotas para utilidades que não vamos usar no front, usei para testar a conexão com o banco de dados
 rotas.post('/habilidades', utilitarios.cadastrarHabilidade);
+
+
+// rotas para o forum
+rotas.get('/forum', forum.listarPerguntas);
+rotas.get('/forum/comentarios', forum.listarComentarios);
+rotas.post('/forum/:id', forum.criarPergunta); // id do usuário
+rotas.post('/forum/comentarios/:id', forum.comentarPergunta); // id do usuário
+
 
 module.exports = rotas
