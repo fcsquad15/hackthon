e62ec2ed005a6515026ae7d529cac1bba64377d1
Eduardo <eduardo.pezzi@fohat.co>
2022-04-13 00:29:32 -0300

    first changes

---
+++ hackthon/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { HeaderContainer } from './styles';
+
+export default function Header(){
+    return(
+        <HeaderContainer>
+            oie
+        </HeaderContainer>
+    )
+}
